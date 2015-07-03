d6f135a8aeec33b12e5deb6904fe18ab34da2d34
li <crischenli@gmail.com>
2015-07-03 16:33:22 +0800

    some refactoring

--- ControllerApp/SizeEstimator.py
+++ ControllerApp/SizeEstimator.py
@@ -3,6 +3,8 @@ __author__ = 'li'
 from flowsizepred import GPRFlowEstimator
 import datetime
 
+class PredictionModels:
+    GPR, NW, RL = range(3)
 
 class SizeEstimator(object):
     def __init__(self):
@@ -21,10 +23,22 @@ class SizeEstimator(object):
         # interface with size estimation
         return size
 
+    def train(self, model=PredictionModels.GPR, trainingset='univ_1_1.csv', modelpath='model.txt'):
+        if model == PredictionModels.GPR:
+            m = self.gflowestm_train(self, trainingset=trainingset, modelpath=modelpath)
+        elif model == PredictionModels.NW:
+            m = self.nflowestm_train(self, trainingset=trainingset, modelpath=modelpath)
+        elif model == PredictionModels.RL:
+            m = self.rflowestm_train(self, trainingset=trainingset, modelpath=modelpath)
+        else:
+            print 'Unknown model {}, cannot train'.format(model)
+        return m
+
     def gflowestm_train(self, trainingset='univ_1_1.csv', modelpath='model.txt'):
         samples = trainingset  # input sample file
         target_name = 'Payload_Bytes'
-        feature_name = 'Server_IP_1:Server_IP_2:Server_IP_3:Server_IP_4:Client_IP_1:Client_IP_2:Client_IP_3:Client_IP_4:Server_Port:Client_Port'
+        feature_name = 'Server_IP_1:Server_IP_2:Server_IP_3:Server_IP_4:' +\
+                       'Client_IP_1:Client_IP_2:Client_IP_3:Client_IP_4:Server_Port:Client_Port'
         # Server_IP_1 ~ Server_IP_4 are the four bytes from an IP address
         feature_type = 'IP:IP:IP:IP:IP:IP:IP:IP:Port:Port'
         feature_weight = '0.2:0.1:0.1:0.1:0.1:0.1'
@@ -36,6 +50,17 @@ class SizeEstimator(object):
         model = gpr.train(samples, target_name, feature_name, feature_type, feature_weight,
                           start, length, model_path=modelpath)
         return model
+
+    def predict(self, flow, model=PredictionModels.GPR):
+        if model == PredictionModels.GPR:
+            fs = self.gflowestm_predict(flow)
+        elif model == PredictionModels.NW:
+            fs = self.nflowestm_predict(flow)
+        elif model == PredictionModels.RL:
+            fs = self.rflowestm_predict(flow)
+        else:
+            print 'Unknown model {}, cannot predict'.format(model)
+        return fs
 
     def gflowestm_predict(self, flow):
         flowsample = self.flowSampleMaker(flow)
@@ -50,6 +75,16 @@ class SizeEstimator(object):
         flowsample = srcIP + ':' + dstIP + ':' + srcPort + ':' + dstPort
         return flowsample
 
+    def update(self, flow, model=PredictionModels.GPR):
+        if model == PredictionModels.GPR:
+            self.gflowestm_update(flow)
+        elif model == PredictionModels.NW:
+            self.nflowestm_update(flow)
+        elif model == PredictionModels.RL:
+            self.rflowestm_update(flow)
+        else:
+            print 'Unknown model {}, cannot predict'.format(model)
+
     def gflowestm_update(self, flow):
         # do this whenever a flow finishes routing and the flow info. is collected.
         flowsample = self.flowSampleMaker(flow)
@@ -63,7 +98,7 @@ class SizeEstimator(object):
 
 def main():
     estm = SizeEstimator()
-    estm.gflowestm_train()
+    # estm.gflowestm_train()
     estm.gflowestm_predict()
 
 
